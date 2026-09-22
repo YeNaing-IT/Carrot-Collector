929bea5f0ede6d6731ef50837be73cfae16b339b
Ye Naing <naing.ye@my.jcu.edu.au>
2026-09-22 23:42:34 +0800

    Version 1.0
    - Joystick movement
    - Win Text

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public FixedJoystick joystick;
+    public float moveSpeed;
+
+    float hInput, vInput; // Get Inputs from joystick
+
+    int score = 0;
+
+    public GameObject winText;
+    public int winScore;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        hInput = joystick.Horizontal * moveSpeed;
+        vInput = joystick.Vertical * moveSpeed;
+
+        transform.Translate(hInput, vInput, 0);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Carrot")
+        {
+            score++;
+
+            Destroy(collision.gameObject);
+
+            if (score >= winScore)
+            {
+                winText.SetActive(true);
+            }
+        }
+
+    }
+}
